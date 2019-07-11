e21cd861be22a202afa8a6bd0019086f28b50159
nqqdegit <13937165721@163.com>
2019-07-11 16:05:31 +0800

    sss

---
+++ client/comment/src/main/java/com/nqq/comment/Application.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C), 2015-2019, 付临门有限公司
+ * FileName: Application
+ * Author:   user
+ * Date:     2019/7/10 15:47
+ * Description: 评论
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.nqq.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈评论〉
+ *
+ * @author user
+ * @create 2019/7/10
+ * @since 1.0.0
+ */
+@SpringCloudApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ client/index/src/main/java/com/nqq/index/Application.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C), 2015-2019, 付临门有限公司
+ * FileName: Application
+ * Author:   user
+ * Date:     2019/7/10 10:37
+ * Description: 主页
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.nqq.index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈主页〉
+ *
+ * @author user
+ * @create 2019/7/10
+ * @since 1.0.0
+ */
+@SpringCloudApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
